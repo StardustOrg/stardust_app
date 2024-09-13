23596934dd4aa0fcd2db5236344f01f8068747e6
Yanna Torres <torres.yanna03@gmail.com>
2024-09-12 21:26:49 -0300

    [Chore] Added splash screen and changed the icon and name displayed

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:get/get.dart';
 import 'package:stardust_app_skeleton/features/authentication/screens/login/login.dart';
 // import 'package:stardust_app_skeleton/features/home/screens/home.dart';
 import 'package:stardust_app_skeleton/utils/theme/theme.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
